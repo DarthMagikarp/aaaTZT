34f233a3e1bc91e23b00ea412ad68981746e63ac
DanielBravoFlores <68356488+DarthMagikarp@users.noreply.github.com>
2024-04-30 23:46:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 app = Flask(__name__)
 @app.route('/shao')
 def shao_mundo():
     return 'Shao Mundo'
+    
 @app.route('/hola')
 def hola_mundo():
     return 'Hola Mundo'
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
