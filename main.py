3fcdba712b0e8f7f1102591ea33c8fc3a91c048a
DanielBravoFlores <68356488+DarthMagikarp@users.noreply.github.com>
2024-04-30 23:24:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+@app.route('/shao')
+def shao_mundo():
+    return 'Shao Mundo'
 @app.route('/hola')
 def hola_mundo():
     return 'Hola Mundo'
 
-@app.route('/shao')
-def shao_mundo():
-    return 'Shao Mundo'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
